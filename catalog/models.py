6c865e3ee4e5bfb37a0dc3ad2865a7feb028cd1d
Persiteo <Persiteo@users.noreply.github.com>
2023-09-03 23:11:32 +0300

    start modeling

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    # preview = models...
+    category = models.CharField(max_length=50)
+    retail_price = models.IntegerField(default = 0)
+    date_initial = models.DateTimeField
+    date_changes = models.DateTimeField
+
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
