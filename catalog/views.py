c7882e915ab4fee22b7e9b5a56b37f39eb3754c8
Persiteo <Persiteo@users.noreply.github.com>
2023-09-03 22:43:33 +0300

    rebuild 19.2, preparation to 20.1

---
+++ catalog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        with open('log.txt', 'a') as f:
+            f.writelines(f'{name} Написал: {message} (Телефон для связи: {phone})/')
+    return render(request, 'catalog/contacts.html')
